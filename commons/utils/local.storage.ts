2885c8bc49c91a6ced78117156d5c135ac6898b2
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-07-22 16:39:12 -0500

    add local storage

---
+++ commons/utils/local.storage.ts
@@ -0,0 +1,25 @@
+export enum LocalStorageKeys {
+    AUTH = "auth"
+}
+
+export const clearLocalStorage = () => {
+    localStorage.clear();
+};
+
+export const localStorageKeyExists = (key: string) => {
+    const result = localStorage.getItem(key);
+    return !!result;
+}
+
+export const getLocalStorage = (key: string) => {
+    const result = localStorage.getItem(key);
+    return !!result && JSON.parse(result);
+};
+
+export const removeLocalStorage = (key: string) => {
+    localStorage.removeItem(key);
+};
+
+export const setLocalStorage = (key: string, value: string) => {
+    localStorage.setItem(key, value);
+};
