06a1e7f1d019a1af36c44d641ccc0d64ca415640
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-07-22 10:42:09 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: "class",
+	theme: {
+		colors: {
+			"primary": "#ffca4c",
+			"secondary": "#1243B0"
+		},
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
