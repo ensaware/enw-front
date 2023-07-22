33da68c4fb21c8ec75b2a1574cfa7eafb4939c08
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-07-22 18:09:40 -0500

    add or update index file

---
+++ commons/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './axios.interceptor';
+export * from './database';
+export * from './local.storage';
+export * from './url.builder';
---
+++ environments/index.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	api: {
+		base: "https://enw-back-vyozdnnbma-rj.a.run.app",
+		services: {
+			authorization: "/{version}/authorization/{provider}",
+			library: "/{version}/library",
+			qrCode: "/{version}/qr-code",
+			user: "/{version}/user",
+		},
+	},
+};
