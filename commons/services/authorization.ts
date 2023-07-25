6eea08f1dccb6e346e5d6817adc893fe89181f55
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-07-24 23:03:30 -0500

    add authorization service

---
+++ commons/services/authorization.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { environment } from "@/environments";
+import { getToken, urlBuilder } from "@/commons/utils";
+import { IToken } from "@/commons/entities";
+
+export const login = (provider: string) => {
+	let url: string = urlBuilder.services(
+		environment.api.services.authorization,
+		{
+			provider: provider,
+			version: "v1",
+		}
+	);
+
+	url = `${url}`;
+
+	window.location.href = url;
+};
+
+
+export const refreshToken = async (provider: string) => {
+	const token: IToken | null = await getToken();
+
+	let url: string = urlBuilder.services(
+		environment.api.services.authorization,
+		{
+			provider: provider,
+			version: "v1",
+		}
+	);
+	url = `${url}/refresh/token`;
+
+	return await axios.post<IToken>(url, {
+		refresh_token: token?.refresh_token,
+	});
+};
