241443c3d4b77b8f87d07d3ad8c0e2e1ef41f07f
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-07-27 10:10:30 -0500

    clean code

--- modules/library/pages/create/create.hook.ts
+++ modules/library/pages/create/create.hook.ts
@@ -2,29 +2,14 @@ import { ChangeEvent, useCallback, useRef, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from 'yup';
 
-import Webcam from "react-webcam";
 import { ICreate } from "../../entities/create.entity";
 import { create } from "@/commons/services";
 import { IViewLibrary } from "@/commons/entities/library.entity";
 
 const useCreateHook = () => {
-	const webcamRef = useRef<Webcam>(null);
-
-	const [imgCamara, setImgCamara] = useState<string | null | undefined>(null);
 	const [selectedImage, setSelectedImage] = useState<File | null>(null);
 	const [library, setLibrary] = useState<IViewLibrary | null>(null);
 	const [loading, setLoading] = useState<boolean>(false);
-
-	const capture = useCallback(() => {
-		const imageSrc = webcamRef.current?.getScreenshot();
-		setImgCamara(imageSrc);
-
-		const imgFile = new File([
-			new Blob([imageSrc ? imageSrc : ""])
-		], "book.png");
-		setSelectedImage(imgFile);
-
-	}, [webcamRef, setImgCamara]);
 
 	const validateSchema = Yup.object().shape({
 		image: Yup.mixed().required("Por favor selecciona una imagen.")
@@ -43,11 +28,13 @@ const useCreateHook = () => {
 			const formData = new FormData();
 			formData.append("image", selectedImage ? selectedImage : "");
 
-			const { data }: { data : IViewLibrary } = await create(formData);
-			setLibrary(data);
-
-			action.resetForm();
-			setLoading(false);
+			try {
+				const { data }: { data : IViewLibrary } = await create(formData);
+				setLibrary(data);
+				action.resetForm();
+			} finally {
+				setLoading(false);
+			}
 		},
 	});
 
@@ -58,32 +45,11 @@ const useCreateHook = () => {
 		}
 	};
 
-	const createMovil = async() => {
-		try {
-			setLoading(true);
-			setImgCamara(null);
-
-			const formData = new FormData();
-			formData.append("image", selectedImage ? selectedImage : "");
-
-			const { data }: { data : IViewLibrary } = await create(formData);
-			setLibrary(data);
-
-			setLoading(false);
-		} catch {
-			setLoading(false);
-		}
-	}
-
 	return {
-		capture,
 		createFormik,
-		createMovil,
-		imgCamara,
 		library,
 		loading,
-		onChangeImage,
-		webcamRef
+		onChangeImage
 	};
 };
 
--- modules/library/pages/create/create.page.tsx
+++ modules/library/pages/create/create.page.tsx
@@ -1,22 +1,15 @@
 "use client";
 
 import { Alert, Button, Card, FileInput, Label } from "flowbite-react"
-import { isMobile } from "react-device-detect";
 import { GiSpellBook } from "react-icons/gi";
 
 import styles from "./create.module.css";
-import Webcam from "react-webcam";
 import { useCreateHook } from "./create.hook";
 
 import { EnwSpinner } from "@/commons/components/spinner";
-import Image from "next/image";
 
 const CreateLibrary = () => {
 	const createHook = useCreateHook();
-
-	const videoConstraints = {
-		facingMode: "environment"
-	};
 
 	return (
 		<section>
@@ -56,66 +49,6 @@ const CreateLibrary = () => {
 							Crear
 						</Button>
 					</form>
-
-				{/* {isMobile ? (
-					<>
-						{createHook.imgCamara ? (
-							<>
-								<Image
-									src={createHook.imgCamara}
-									alt="Prueba"
-									width={createHook.webcamRef.current?.getCanvas()?.width}
-									height={createHook.webcamRef.current?.getCanvas()?.height}
-								/>
-
-								<Button className={styles.button} onClick={createHook.createMovil}>
-									Crear
-								</Button>
-							</>
-						): (
-							<>
-								<Webcam
-									audio={false}
-									screenshotQuality={1}
-									ref={createHook.webcamRef}
-									videoConstraints={videoConstraints}
-								/>
-
-								<Button onClick={createHook.capture} className={styles.buttonCapture}>
-									Tomar foto
-								</Button>
-							</>
-						)}
-					</>
-				): (
-					<form
-						className={styles.form}
-						autoComplete="off"
-						onSubmit={createHook.createFormik.handleSubmit}
-					>
-						<div>
-							<div className="mb-2 block">
-								<Label
-									htmlFor="file"
-									value="Subir foto"
-								/>
-							</div>
-
-							<FileInput
-								accept='image/*'
-								color={createHook.createFormik.errors.image ? "failure" : ""}
-								id="file"
-								helperText={createHook.createFormik.errors.image ? createHook.createFormik.errors.image : "Por favor subir la foto del código de barra de libro en muy buena calidad."}
-								name="image"
-								onChange={createHook.onChangeImage}
-							/>
-						</div>
-
-						<Button className={styles.button} type="submit">
-							Crear
-						</Button>
-					</form>
-				)} */}
 			</Card>
 		</section>
 	)
