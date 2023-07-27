0a4ff660f1fe824becba2a9a6f34f958e09c1ed9
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-07-27 02:19:41 -0500

    fix create

--- modules/library/pages/create/create.page.tsx
+++ modules/library/pages/create/create.page.tsx
@@ -29,7 +29,35 @@ const CreateLibrary = () => {
 					</Alert>
 				)}
 
-				{isMobile ? (
+				<form
+						className={styles.form}
+						autoComplete="off"
+						onSubmit={createHook.createFormik.handleSubmit}
+					>
+						<div>
+							<div className="mb-2 block">
+								<Label
+									htmlFor="file"
+									value="Subir foto"
+								/>
+							</div>
+
+							<FileInput
+								accept='image/*'
+								color={createHook.createFormik.errors.image ? "failure" : ""}
+								id="file"
+								helperText={createHook.createFormik.errors.image ? createHook.createFormik.errors.image : "Por favor subir la foto del código de barra de libro en muy buena calidad."}
+								name="image"
+								onChange={createHook.onChangeImage}
+							/>
+						</div>
+
+						<Button className={styles.button} type="submit">
+							Crear
+						</Button>
+					</form>
+
+				{/* {isMobile ? (
 					<>
 						{createHook.imgCamara ? (
 							<>
@@ -87,7 +115,7 @@ const CreateLibrary = () => {
 							Crear
 						</Button>
 					</form>
-				)}
+				)} */}
 			</Card>
 		</section>
 	)
